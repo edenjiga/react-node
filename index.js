5afe838fb8dd991de26d61a0657d27635b014f93
Edgar <edenjiga@gmail.com>
2018-04-29 14:55:20 -0500

    finish second module

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
-require('./services/passport');
-const express = require('express');
 
+const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(process.env.MONGODB);
 const app = express();
 
+app.use(passport.initialize());
+app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,18 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -8,8 +21,18 @@ passport.use(
       clientSecret: process.env.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record!
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
